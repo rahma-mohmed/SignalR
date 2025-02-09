d9bf1fdf8d7c9569f91af88d96b5ac7e74705d51
rahma-mohmed <engrahma1422003@gmail.com>
2025-02-09 05:58:28 +0200

    add employee model real time employee table

---
+++ RealTimeTable/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace RealTimeTable.Models
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Position { get; set; }
+		public int Age { get; set; }
+	}
+}
