1940a7b995226bb37bef84f51b3d74963019ac23
rahma-mohmed <engrahma1422003@gmail.com>
2025-02-09 05:57:15 +0200

    real time employee table add employee controller

---
+++ RealTimeTable/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealTimeTable.Data;
+
+namespace RealTimeTable.Controllers
+{
+	public class EmployeeController : Controller
+	{
+		private readonly AppDbContext _context;
+
+		public EmployeeController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var employees = await _context.Employees.ToListAsync();
+			return View(employees);
+		}
+	}
+}
