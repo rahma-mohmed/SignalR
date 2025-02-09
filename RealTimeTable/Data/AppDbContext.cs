8a1c0ca6dad0424ab9ab8456e6c225c763f015d0
rahma-mohmed <engrahma1422003@gmail.com>
2025-02-09 05:57:29 +0200

    add DbContext

---
+++ RealTimeTable/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealTimeTable.Models;
+
+namespace RealTimeTable.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Employee> Employees { get; set; }
+	}
+}
