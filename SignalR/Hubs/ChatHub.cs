594445a97fcbb5adfe176ac21709911fe6bf68a7
rahma-mohmed <engrahma1422003@gmail.com>
2025-02-09 17:54:34 +0200

    Add group function in chat

--- SignalR/Hubs/ChatHub.cs
+++ SignalR/Hubs/ChatHub.cs
@@ -4,6 +4,7 @@ namespace SignalR.Hubs
 {
 	public class ChatHub : Hub
 	{
+		#region Paramaters as premative data types
 		public void SendMessage(string name, string message)
 		{
 			// save in database
@@ -11,5 +12,43 @@ namespace SignalR.Hubs
 			// send to all online clients
 			Clients.All.SendAsync("newMessage", name, message);
 		}
+		#endregion
+
+		public void joingroup(string groupname, string name)
+		{
+			// add group
+			Groups.AddToGroupAsync(Context.ConnectionId, groupname);
+
+			// send to all online clients (broadcasting)
+			Clients.OthersInGroup(groupname).SendAsync("newmember", name, groupname);
+			Clients.Caller.SendAsync("member", name, groupname);
+		}
+
+		public void leavegroup(string groupname, string name)
+		{
+			// remove group
+			Groups.RemoveFromGroupAsync(Context.ConnectionId, groupname);
+			// send to all online clients (broadcasting)
+			Clients.OthersInGroup(groupname).SendAsync("leavemember", name, groupname);
+		}
+
+		[HubMethodName("sendtogroup")]
+		public void sendToGroup(string groupname, string name, string message)
+		{
+			Clients.Group(groupname).SendAsync("newgroupmessage", name, groupname, message);
+		}
+
+		#region Hub events
+		public override Task OnConnectedAsync()
+		{
+			string connectionId = Context.ConnectionId;
+			return base.OnConnectedAsync();
+		}
+
+		public override Task OnDisconnectedAsync(Exception? exception)
+		{
+			return base.OnDisconnectedAsync(exception);
+		}
+		#endregion
 	}
 }
