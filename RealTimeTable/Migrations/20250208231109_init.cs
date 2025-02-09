5b4f0baf30b68bec90f3745290247c949dafae62
rahma-mohmed <engrahma1422003@gmail.com>
2025-02-09 05:57:56 +0200

    add migration files

---
+++ RealTimeTable/Migrations/20250208231109_init.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealTimeTable.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "age",
+                table: "Employees",
+                newName: "Age");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "Employees",
+                newName: "age");
+        }
+    }
+}
