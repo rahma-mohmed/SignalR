38ff4cb56f43d0dc136cacb0de925ad02cfa168a
rahma-mohmed <engrahma1422003@gmail.com>
2025-02-09 05:57:43 +0200

    employee hub

---
+++ RealTimeTable/Hubs/AddEmployeeHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealTimeTable.Data;
+using RealTimeTable.Models;
+
+namespace RealTimeTable.Hubs
+{
+	public class AddEmployeeHub : Hub
+	{
+		private readonly AppDbContext _context;
+
+		public AddEmployeeHub(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task AddEmployee(Employee employee)
+		{
+
+			// save in database
+			await _context.Employees.AddAsync(employee);
+			await _context.SaveChangesAsync();
+
+			// send to all online clients
+			await Clients.All.SendAsync("newEmployee", employee);
+		}
+	}
+}
